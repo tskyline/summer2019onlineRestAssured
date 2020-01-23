5b9c0f7d6973f8b6a2ec0633eec272f37853daab
Tunahan Kirtil <tuna1@me.com>
2020-01-23 11:47:26 -0500

    1st

--- src/test/java/com/automation/tests/day2/ORDSTests.java
+++ src/test/java/com/automation/tests/day2/ORDSTests.java
@@ -7,6 +7,9 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class ORDSTests
 {
+    //DATA is coming from SQL Oracle dbase to this web service
+    private String baseURI="http://ec2-52-90-141-57.compute-1.amazonaws.com:1000/ords/hr/";
+
     @Test
     public void test1()
     {
