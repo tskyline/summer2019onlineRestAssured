66d65d95fe6e341595bd18ba2daedc3f59cf335e
Tunahan Kirtil <tuna1@me.com>
2020-01-23 11:39:11 -0500

    Hello

--- src/test/java/com/automation/tests/day2/ORDSTests.java
+++ src/test/java/com/automation/tests/day2/ORDSTests.java
@@ -1,6 +1,15 @@
 package com.automation.tests.day2;
 
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.*;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class ORDSTests
 {
+    @Test
+    public void test1()
+    {
 
+    }
 }
